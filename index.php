e28f3a4b4650b95884f9c8780aab63e5210f8295
Myles Beardsmore <mediabeastnz@gmail.com>
2013-05-06 16:04:43 +1200

    Initial commit of website.

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8"/>
+<title>Myles Beardsmore. Web Developer.</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<link rel="stylesheet" href="./assets/style.css" type="text/css"/>
+<link rel="shortcut icon" href="./assets/32by32.ico" type="image/x-icon"/>
+<link rel="apple-touch-icon-precomposed" href="./assets/48by48.ico"/>
+<meta name="description" content="Myles Beardsmore is a PHP developer based in New Zealand."/>
+<meta name="author" content="Myles Beardsmore"/>
+
+<script type="text/javascript" src="//use.typekit.net/unu5mnz.js"></script>
+<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+ 
+</head>
+<body class="about">
+	<div id="topbar">
+		<div class="profilepic">
+			<div class="profilewrap">
+				<a href="/"><img src="./assets/me.jpg" alt="Myles Beardsmore"/></a>
+			</div>
+			<h2 class="profilename"><a href="/" class="profilename" title="This is me.">Myles Beardsmore</a></h2>
+		</div>
+		<div class="sociallinks">
+			<a href="https://twitter.com/mediabeastnz" target="_blank" aria-hidden="true" class="icon-twitter-2" title="Twiiter"></a> 
+			<a href="https://github.com/mediabeastnz" target="_blank" aria-hidden="true" class="icon-github" title="Github"></a> 
+			<a href="http://instagram.com/mediabeastnz" target="_blank" aria-hidden="true" class="icon-instagram" title="Instagram"></a> 
+			<a href="http://linkedin.com/pub/myles-beardsmore/37/49b/889" target="_blank" aria-hidden="true" class="icon-linkedin" title="LinkedIn"></a>
+		</div>
+	</div>  
+	<div class="container" id="feature">
+		<div class="content">
+			<h1 class="posttitle">About</h1>
+			<p>Hey, I&#8217;m Myles Beardsmore, a PHP Developer based in New Zealand. I build innovative and creative products.</p>
+		</div>
+	</div>
+	<div class="container">
+		<div class="content">
+			<p></p>
+			<p><strong>Where can you find me?</strong><br />
+				You can tweet me on <a href='http://www.twitter.com/andytlr/'>Twitter</a>, check out my activity on <a href="">Github</a>, view photographs I've taken on <a href="">Instagram</a> or look at my profile on <a href="">LinkedIn</a>.</p>
+
+			<p>If you would like to work together, send me a <a href="mailto:myles@mylesb.me">message</a>.</p>
+		</div>
+	</div>
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-40671841-1', 'mylesb.me');
+	  ga('send', 'pageview');
+
+	</script>
+</body>
+</html>
